f32e99a55182227f486ad3f304add9230af49c58
Tennyson Ben-kalio <69404440+Benkalio@users.noreply.github.com>
2021-10-04 18:32:04 +0100

    Concluded Javascript

---
+++ script.js
@@ -0,0 +1,34 @@
+const header = document.getElementById("header");
+const title = document.getElementById("title");
+const excerpt = document.getElementById("excerpt");
+const profile_img = document.getElementById("profile_img");
+const name = document.getElementById("name");
+const date = document.getElementById("date");
+
+const animated_bgs = document.querySelectorAll(".animated-bg");
+const animated_bgs_texts = document.querySelectorAll(".animated-bg-text");
+
+const today = new Date().getFullYear();
+// dd = String(today.getDate()).padStart(2, "0"),
+// mm = String(today.getMonth() + 1).padStart(2, "0"),
+// yyyy = today.getFullYear();
+
+document.write(today);
+//SETTING TIMEOUT TO DELAY BEFORE CALLING getData() function
+setTimeout(getData(), 3000);
+
+function getData() {
+  header.innerHTML =
+    '<img src="https://images.unsplash.com/photo-1541480601022-2308c0f02487?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt=""/>';
+  title.innerHTML = "Some title here";
+  excerpt.innerHTML =
+    "Some words can occupy this space for now until I have dynamically created the Javascript.";
+  profile_img.innerHTML =
+    '<img src="https://randomuser.me/api/portraits/men/45.jpg" alt="users" />';
+  name.innerHTML = "Joseph Yobo";
+  date.innerHTML = `${today}`;
+
+  //ADDING THE ANIMATION CLASS to loop through and remove the class from the html tags
+  animated_bgs.forEach((bg) => bg.classList.remove("animated-bg"));
+  animated_bgs_texts.forEach((bg) => bg.classList.remove("animated-bg-text"));
+}
