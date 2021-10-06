97d5e42dad757c5b6bda40537d94b4fa6ae7aa1c
Tennyson Ben-kalio <69404440+Benkalio@users.noreply.github.com>
2021-10-06 22:33:22 +0100

    ADJUSTMENTS TO SCRIPT VISIBLE COMMENTS

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ const today = new Date().getFullYear();
 setTimeout(getData(), 3000);
 
 function getData() {
+  //DISPLAYING HEADER IMAGE
   header.innerHTML =
     '<img src="https://images.unsplash.com/photo-1541480601022-2308c0f02487?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt=""/>';
   title.innerHTML = "Some title here";
