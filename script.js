99cbc2e79846765bfd74503469c3cca2bd2e5205
Tennyson Ben-kalio <69404440+Benkalio@users.noreply.github.com>
2021-10-06 22:33:56 +0100

    NEW COMMENTS

--- script.js
+++ script.js
@@ -20,6 +20,7 @@ function getData() {
   //DISPLAYING HEADER IMAGE
   header.innerHTML =
     '<img src="https://images.unsplash.com/photo-1541480601022-2308c0f02487?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt=""/>';
+  //DISPLAYING TITLE
   title.innerHTML = "Some title here";
   excerpt.innerHTML =
     "Some words can occupy this space for now until I have dynamically created the Javascript.";
